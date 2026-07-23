44aaf716423ce00c865cbea069e3647084170398
abhinav-codes123 <abhinavchaudhary484@gmail.com>
2026-07-23 19:38:10 +0530

    working on register user

--- project3/src/controllers/user.controller.js
+++ project3/src/controllers/user.controller.js
@@ -1,9 +1,25 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/apiError.js";
 
 const registerUser= asyncHandler( async (req,res)=>{
-    res.status(200).json({
-        message: "ok"
-    })
+    // res.status(200).json({
+    //     message: "ok"
+    // })
+
+    const {fullName, email, username, password}= req.body
+
+    console.log("email : ", email)
+    // console.log("password : ",password)
+
+    // if(fulllName===""){
+    //     throw new ApiError(400,"fullname is required");
+    // }
+
+    if(
+        [fullName,email,username,password].some((field)=>
+        field?.trim()==="")){
+            throw new ApiError(400,"all fields are required");
+        }
 })
 
 export {registerUser}
